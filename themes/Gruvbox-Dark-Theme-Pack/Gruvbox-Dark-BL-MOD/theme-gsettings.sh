e428eadd01aebb5b991fd35860950b4ca9e1805e
Atomjoy <atomjoy.official@gmail.com>
2024-01-11 18:43:46 +0100

    Gnome theme

--- themes/Gruvbox-Dark-Theme-Pack/Gruvbox-Dark-BL-MOD/theme-gsettings.sh
+++ themes/Gruvbox-Dark-Theme-Pack/Gruvbox-Dark-BL-MOD/theme-gsettings.sh
@@ -7,13 +7,13 @@ sudo apt install gnome-tweaks
 # Add theme to ~/.themes then load theme from ~/.bashrc  add on top (Nautilus files)
 cp -rf ~/.themes/Gruvbox-Dark-BL-MOD/gtk-4.0 ~/.config/gtk-4.0
 
-# Create if gtk.css file does not exist
+# Create if gtk.css file does not exist (usuwa się gtk.css z .config/gtk-4.0 po wylogowaniu)
 cp -rf ~/.config/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk.css
 
 # Set
 GTK_THEME=Gruvbox-Dark-BL-MOD
 
-# Run from terminal
+# Run from terminal (a to zadziała za kilkanaście sekund :D:D:D:D idiotyczne)
 gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Dark-BL-MOD
 gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.wm.preferences theme Gruvbox-Dark-BL-MOD
