e9d6d86c3b23909e1ac7b3287df89fd99b5739bd
Atomjoy <atomjoy.official@gmail.com>
2024-01-11 14:03:42 +0100

    Gnome theme

--- themes/Gruvbox-Dark-Theme-Pack/Gruvbox-Dark-BL-MOD/theme-gsettings.sh
+++ themes/Gruvbox-Dark-Theme-Pack/Gruvbox-Dark-BL-MOD/theme-gsettings.sh
@@ -1,14 +1,15 @@
 # Desktop scale 125%
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 
-# Load theme
+# Load theme https://wiki.archlinux.org/title/GTK
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.wm.preferences theme Gruvbox-Dark-BL-MOD
 gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Dark-BL-MOD
+GTK_THEME=Gruvbox-Dark-BL-MOD:dark
+echo "export GTK_THEME=Gruvbox-Dark-BL-MOD:dark" >> ~/.profile
 
 # Change theme icons with gtk3/4 
 sudo apt install gnome-tweaks
 
 # Load theme from ~/.bashrc (gtk4 files window titlebars) add on top
 cp -rf ~/.themes/Gruvbox-Dark-BL-MOD/gtk-4.0 ~/.config/gtk-4.0
-
-
