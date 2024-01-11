242817a0987f51d33579769aa323a9a8346eb14b
Atomjoy <atomjoy.official@gmail.com>
2024-01-11 18:39:05 +0100

    Gnome theme

--- themes/Gruvbox-Dark-Theme-Pack/Gruvbox-Dark-BL-MOD/theme-gsettings.sh
+++ themes/Gruvbox-Dark-Theme-Pack/Gruvbox-Dark-BL-MOD/theme-gsettings.sh
@@ -1,15 +1,23 @@
 # Desktop scale 125%
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 
-# Load theme https://wiki.archlinux.org/title/GTK
-gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-gsettings set org.gnome.desktop.wm.preferences theme Gruvbox-Dark-BL-MOD
-gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Dark-BL-MOD
-GTK_THEME=Gruvbox-Dark-BL-MOD:dark
-echo "export GTK_THEME=Gruvbox-Dark-BL-MOD:dark" >> ~/.profile
-
 # Change theme icons with gtk3/4 
 sudo apt install gnome-tweaks
 
-# Load theme from ~/.bashrc (gtk4 files window titlebars) add on top
+# Add theme to ~/.themes then load theme from ~/.bashrc  add on top (Nautilus files)
 cp -rf ~/.themes/Gruvbox-Dark-BL-MOD/gtk-4.0 ~/.config/gtk-4.0
+
+# Create if gtk.css file does not exist
+cp -rf ~/.config/gtk-4.0/gtk-dark.css ~/.config/gtk-4.0/gtk.css
+
+# Set
+GTK_THEME=Gruvbox-Dark-BL-MOD
+
+# Run from terminal
+gsettings set org.gnome.desktop.interface gtk-theme Gruvbox-Dark-BL-MOD
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+gsettings set org.gnome.desktop.wm.preferences theme Gruvbox-Dark-BL-MOD
+
+# !!! Co za linux-owe cudaki !!!
+
+
